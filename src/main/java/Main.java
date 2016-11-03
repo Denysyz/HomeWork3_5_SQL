0b19b115b9366d25e7e1f9acda5785d4204a6893
dende <kawoonn@gmail.com>
2016-11-04 01:24:17 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,76 @@
+
+
+import java.sql.*;
+import java.util.Scanner;
+
+/**
+ * Created by dende on 04.11.2016.
+ */
+public class Main {
+    static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/mydb";
+    static final String DB_USER = "root";
+    static final String DB_PASSWORD = "0000";
+    static Connection con;
+    public static void main(String[] args) {
+
+        try {
+            con = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("1 - Add apartment");
+        System.out.println("2 - Search apartment");
+        Scanner sc = new Scanner(System.in);
+        int v  = sc.nextInt();
+        if( v == 1){
+            addApartment();
+        }else if(v == 2){
+            serch();
+        }else{
+            System.out.println("error comand");
+        }
+
+
+
+
+
+
+    }
+    private static void addApartment(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter region");
+        String region = sc.nextLine();
+        System.out.println("Enter adress");
+        String adress = sc.nextLine();
+        System.out.println("Enter area");
+        int area = sc.nextInt();
+        System.out.println("Enter rooms");
+        int rooms = sc.nextInt();
+        System.out.println("Enter price");
+        int price = sc.nextInt();
+        sc.close();
+        try {
+            Statement st = con.createStatement();
+            st.execute("insert into apartments values('" + region + "', '" + adress + "', " + area + ", " +
+                    rooms + ", " + price +");");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void serch(){
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM apartments");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                for(int i = 1;i <= 5; i++){
+                    System.out.print(rs.getString(i) + ", ");
+
+                }
+                System.out.println();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
