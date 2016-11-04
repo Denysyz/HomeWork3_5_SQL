08d3cc5700a726ae3db2a4b02d75c773c619b174
dende <kawoonn@gmail.com>
2016-11-04 13:12:26 +0200

    04.11.2016 - 13:12 add new metods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -19,6 +21,8 @@ public class Main {
         }
         System.out.println("1 - Add apartment");
         System.out.println("2 - Search apartment");
+        System.out.println("3 - Sort Max to Min");
+        System.out.println("4 - Sort Min to Max");
         System.out.println("0 - Exit");
         while (true){
             Scanner sc = new Scanner(System.in);
@@ -26,8 +30,13 @@ public class Main {
             if( v == 1){
                 addApartment();
             }else if(v == 2){
-                serch();
+                viem();
+            }else if( v == 3){
+                sort("max");
+            }else if(v == 4){
+                sort("min");
             }else if (v == 0){
+                System.out.println("Exit!!!");;
                 break;
             }else{
                 System.out.println("error command");
@@ -56,18 +65,45 @@ public class Main {
         }
         System.out.println("Succesfull");
     }
-    private static void serch(){
+    private static void viem(){
         try {
             PreparedStatement ps = con.prepareStatement("SELECT * FROM apartments");
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 for(int i = 1;i <= 5; i++){
                     System.out.print(rs.getString(i) + ", ");
-
                 }
                 System.out.println();
             }
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void sort(String m){
+        PreparedStatement ps2;
+        ArrayList<Integer> arr = new ArrayList<>();
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM apartments");
+            ResultSet rs = ps.executeQuery();
+            for(int i = 0; rs.next(); i++){
+                arr.add(rs.getInt(5));
+            }
+            Collections.sort(arr);
+            for(int i = 0; i < arr.size();i++){
+                if(m.equals("max")){
+                    ps2 = con.prepareStatement("SELECT * FROM apartments WHERE price LIKE " + arr.get(arr.size() - i - 1));
+                }else{
+                    ps2 = con.prepareStatement("SELECT * FROM apartments WHERE price LIKE " + arr.get(i));
+                }
+                ResultSet rs2 = ps2.executeQuery();
+                while (rs2.next()) {
+                    for(int i2 = 1;i2 <= 5; i2++){
+                        System.out.print(rs2.getString(i2) + ", ");
+                    }
+                    System.out.println();
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
